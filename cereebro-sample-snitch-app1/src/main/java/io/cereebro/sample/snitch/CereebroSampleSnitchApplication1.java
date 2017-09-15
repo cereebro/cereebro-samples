780812994ab316953d3251d5957a99b6494b9587
michaeltecourt <michael.tecourt@gmail.com>
2017-09-15 21:55:07 +0200

    Add a sample controller to debug Cereebro #97

--- cereebro-sample-snitch-app1/src/main/java/io/cereebro/sample/snitch/CereebroSampleSnitchApplication1.java
+++ cereebro-sample-snitch-app1/src/main/java/io/cereebro/sample/snitch/CereebroSampleSnitchApplication1.java
@@ -18,6 +18,8 @@ package io.cereebro.sample.snitch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -25,6 +27,16 @@ public class CereebroSampleSnitchApplication1 {
 
     public static void main(String[] args) {
         SpringApplication.run(CereebroSampleSnitchApplication1.class, args);
+    }
+
+    @RestController
+    public static class HelloController {
+
+        @GetMapping
+        public String hello() {
+            return "hello";
+        }
+
     }
 
 }
